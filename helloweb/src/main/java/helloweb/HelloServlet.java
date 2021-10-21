0e9a70db79012553269155cfe2d996fce45fb99a
hanjoohyung <ghd167@naver.com>
2021-10-21 12:10:37 +0900

    servlet-practices

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -16,6 +16,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
+		
 		response.setContentType("text/html; charset=utf-8");
 
 		PrintWriter pw = response.getWriter();
